0c35da333cd93603751fe380889b255e3634922c
Travis Wise <richardtwise@gmail.com>
2017-03-12 19:08:29 -0600

    reformatted code in files

--- JS/script.js
+++ JS/script.js
@@ -6,35 +6,29 @@ $(document).ready(function () {
 
         $.ajax({
             type: "GET",
-            url: 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=' + artist + '&api_key=3c60da92d4028ba77acda77715e046d5&format=json&callback=?',
+            url: 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + artist + '&api_key=3c60da92d4028ba77acda77715e046d5&format=json&callback=?',
             dataType: 'json',
+            cache: false,
             success: function (data) {
-                for (var i = 0; i < data.results.albummatches.album.length; i++) {
-                    var row = $('<tr><td> <button class=" +"addbtn" +">Add</button> </td><td>' + data.results.albummatches.album[i].artist + '</td><td>' + data.results.albummatches.album[i].name + '</td><td>' + data.results.albummatches.album[i].url + '</td></tr>');
+                for (var i = 0; i < data.topalbums.album.length; i++) {
+                    var row = $('<tr><td> <button class=" +"addbtn" +">Add</button> </td><td>' + data.topalbums.album[i].artist.name + '</td><td>' + data.topalbums.album[i].name + '</td><td>' + data.topalbums.album[i].playcount + '</td></tr>');
                     $('#albumTable').append(row);
                 }
+
             }
         });
+
     });
+
 });
 
-
-
-
-
-
-// function Album(title, artist, numberOfTracks, releaseDate) {
-//     this.title = title;
-//     this.artist = artist;
-//     this.numberOfTracks = numberOfTracks;
-//     this.releaseDate = releaseDate;
-
+var albumPlaylist = new Array();
 
 var playlist = function () { };
 
 playlist.prototype.myPlaylistArray = new Array();
 
-playlist.prototype.addAlbum = function (album) { //accepts album object 
+playlist.prototype.addAlbum = function (album) {
     for (i = 0; i < this.myPlaylistArray.length; i++) {
         if (this.myPlaylistArray[i].title == album.title) {
             return false;
@@ -44,7 +38,7 @@ playlist.prototype.addAlbum = function (album) { //accepts album object
     return true;
 };
 
-playlist.prototype.removeAlbumByTitle = function (title) { //accepts string
+playlist.prototype.removeAlbumByTitle = function (title) {
     for (var i = 0; i < this.myPlaylistArray.length; i++) {
         if (this.myPlaylistArray[i].title == title) {
             this.myPlaylistArray.splice(i, 1);
@@ -53,15 +47,8 @@ playlist.prototype.removeAlbumByTitle = function (title) { //accepts string
     }
     return false
 }
-// var albumLength = this.myPlaylistArray.length;
-//   this.myPlaylistArray.map((album, index)=>{
-//     if (album.title == title){
-//       this.myPlaylistArray.splice(index, 1);
-//     }
-//   })
-//   return (albumLength ==  this.myPlaylistArray.length) ? (false) : (true);
-// };
-playlist.prototype.removeAlbumByArtist = function (artist) { //accepts string
+
+playlist.prototype.removeAlbumByArtist = function (artist) {
     for (var i = 0; i < this.myPlaylistArray.length; i++) {
         if (this.myPlaylistArray[i].artist == artist) {
             this.myPlaylistArray.splice(i, 1);
@@ -70,20 +57,13 @@ playlist.prototype.removeAlbumByArtist = function (artist) { //accepts string
     }
     return false
 }
-// var albumLength = this.myPlaylistArray.length;
-//   this.myPlaylistArray.map((album, index)=>{
-//     if (album.artist == artist){
-//       this.myPlaylistArray.splice(index, 1);
-//     }
-//   })
-//   return (albumLength ==  this.myPlaylistArray.length) ? (false) : (true);
-//};
-playlist.prototype.getRandomAlbum = function () { //accepts string
+
+playlist.prototype.getRandomAlbum = function () {
     var randomAlbum = Math.floor(Math.random() * this.myPlaylistArray.length);
     return this.myPlaylistArray.length <= 0 ? null : this.myPlaylistArray[randomAlbum];
 };
 
-playlist.prototype.getAlbumByTitle = function (title) { //accepts string
+playlist.prototype.getAlbumByTitle = function (title) {
     var titleArray = new Array();
     for (var i = 0; i < this.myPlaylistArray.length; i++) {
         if (this.myPlaylistArray[i].title.indexOf(title) >= 0)
@@ -92,7 +72,7 @@ playlist.prototype.getAlbumByTitle = function (title) { //accepts string
     return titleArray;
 }
 
-playlist.prototype.getAlbumsByArtist = function (artist) { //accepts string
+playlist.prototype.getAlbumsByArtist = function (artist) {
     var artistArray = new Array();
     for (var i = 0; i < this.myPlaylistArray.length; i++) {
         if (this.myPlaylistArray[i].artist.indexOf(artist) >= 0)
@@ -100,7 +80,7 @@ playlist.prototype.getAlbumsByArtist = function (artist) { //accepts string
     }
     return artistArray;
 };
-playlist.prototype.addAlbums = function (albums) { //accepts array
+playlist.prototype.addAlbums = function (albums) {
     var num = 0;
     for (i = 0; i < albums.length; i++) {
         if (this.addAlbum(albums[i])) {
@@ -109,10 +89,6 @@ playlist.prototype.addAlbums = function (albums) { //accepts array
     }
     return num;
 };
-// albums.map((album) => {
-//     this.myPlaylistArray.push(album)
-// })
-// return this.myPlaylistArray;
 
 playlist.prototype.getArtists = function () {
     var artists = new Array();
@@ -124,24 +100,10 @@ playlist.prototype.getArtists = function () {
     return artists;
 }
 
-playlist.prototype.getRandomArtistName = function () { //accepts array
+playlist.prototype.getRandomArtistName = function () {
     var randomArtist = Math.floor(Math.random() * this.myPlaylistArray.length);
     return this.myPlaylistArray[randomArtist].artist;
 };
 
 var playlist1 = new playlist();
 
-            // var album1 = new Album("Dark Side of the Moon", "Pink Floyd", 9, "1973-3-1");
-            // var album2 = new Album("Sgt. Pepper's Lonely Hearts Club Band", "The Beatles", 13, "1967-6-1");
-            // var album3 = new Album("Led Zeppelin IV (aka ZOSO)", "Led Zeppelin", 8, "1971-11-8");
-            // var album4 = new Album("Nevermind", "Nirvana", 12, "1991-9-24");
-            // var album5 = new Album("Ok Computer", "Radiohead", 12, "1997-5-21");
-            // var album6 = new Album("The Wall", "Pink Floyd", 26, "1979-11-30");
-            // playlist1.addAlbum(album1);
-            // playlist1.addAlbum(album2);
-            // playlist1.addAlbum(album3);
-            // playlist1.addAlbum(album4);
-            // playlist1.addAlbum(album5);
-            // playlist1.addAlbum(album6);
-
-            //localStorage.setItem("albums", JSON.stringify(playlist1.myPlaylistArray));
